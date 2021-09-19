90083df2b850315fc97551e2f5acfd07af34edb1
Eduardo Wiater <kpooh7766@gmail.com>
2021-09-19 11:37:40 +0200

    autoformato

--- src/components/Spaceships.js
+++ src/components/Spaceships.js
@@ -1,36 +1,24 @@
-import React , {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import SingleSpaceship from "./SingleSpaceship";
 
+export default function Spaceships() {
+  const [listadoNaves, setListadoNaves] = useState({
+    results: [],
+  });
 
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://swapi.dev/api/starships/");
+      const responseBody = await response.json();
+      setListadoNaves(responseBody);
+    })();
+  }, []);
 
-export default function Spaceships(){
-
-const [listadoNaves, setListadoNaves ] = useState({
-    results: []
-});
-
-useEffect(()=>{
-
-   (async ()=>{
-    const response = await fetch('https://swapi.dev/api/starships/')
-    const responseBody = await response.json()
-    setListadoNaves(responseBody)
-   })()
-
-},[])
-   
-    return (
-        
-        <ul>
-            {listadoNaves.results.map( nave => {
-                return (
-                    <li key={nave.name}>
-                        {nave.name}
-                        <br/>
-                        {nave.model}
-                        
-                    </li>
-                )
-            }) }
-        </ul>
-    )
+  return (
+    <ul>
+      {listadoNaves.results.map((nave) => {
+        return <SingleSpaceship key={nave.name} {...nave} />;
+      })}
+    </ul>
+  );
 }
