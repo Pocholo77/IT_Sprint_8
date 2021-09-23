155191cdb0fd39541301daceada3df416431c04f
Eduardo Wiater <kpooh7766@gmail.com>
2021-09-23 11:33:54 +0200

    Feat: Move out the function fetch to App

--- src/components/Spaceships.js
+++ src/components/Spaceships.js
@@ -1,19 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SingleSpaceship from "./SingleSpaceship";
 
-export default function Spaceships() {
-  const [listadoNaves, setListadoNaves] = useState({
-    results: [],
-  });
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://swapi.dev/api/starships/");
-      const responseBody = await response.json();
-      setListadoNaves(responseBody);
-    })();
-  }, []);
-
+export default function Spaceships({listadoNaves}) {
+  console.log(listadoNaves)
   return (
     <ul>
       {listadoNaves.results.map((nave) => {
