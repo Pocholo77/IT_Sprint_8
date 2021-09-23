a0bd5b3cdb329f07d6f3aefc0b4120aaa09d486e
Eduardo Wiater <kpooh7766@gmail.com>
2021-09-23 11:35:06 +0200

    Feat: Get all fetch function call to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React , { useState, useEffect} from "react";
 import Spaceships from "./components/Spaceships";
 
+
+
+
 function App() {
+
+  const [listadoNaves, setListadoNaves] = useState({
+    results: [],
+  });
+
+  const peticion = async (url) => {
+    const response = await fetch(url)
+    return response.json();
+    //return response     
+  }
+  
+
+  useEffect(() => {
+    (async () => {
+      const res = await peticion("https://swapi.dev/api/starships/?page=2");
+      setListadoNaves(res);
+    })();
+  }, []);
+
+
   return (
     <div className="App">
-      <Spaceships />
+      <Spaceships listadoNaves={listadoNaves}/>
     </div>
   );
 }
