955f4b0fd8a5c28361725be3556286bdcad27a60
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-10 19:27:29 +0200

    Style: Create folder for ships images

---
+++ src/maps/spaceships.js
@@ -0,0 +1,17 @@
+
+export default {
+    "CR90 corvette": "/spaceships/TantiveIV-TSWB.png",
+    "Star Destroyer": "/spaceships/ImperialClassStarDestroyer-TSWB.png",
+    "Millennium Falcon": "/spaceships/Millennium_Falcon_Fathead_TROS.png",
+    "Sentinel-class landing craft": "/spaceships/Sentinel_negvv.png",
+    "Death Star": "/spaceships/DSI_hdapproach.png",
+    "Y-wing": "/spaceships/Y-wing.png",
+    "X-wing": "/spaceships/X-wing_Fathead.png",
+    "TIE Advanced x1": "/spaceships/SWBF_TIE_Avanzado_x1.png",
+    "Executor": "/spaceships/Executor_BF2.png",
+    "Rebel transport": "/spaceships/GR-75_Medium_Transport_TAEtrivia.png",
+    "Slave 1": "/spaceships/Slave_I_DICE.png",
+    "Imperial shuttle": "/spaceships/ImperialShuttle-DB.png",
+    "EF76 Nebulon-B escort frigate": "/spaceships/NebulonB-SWS.png",
+
+}
